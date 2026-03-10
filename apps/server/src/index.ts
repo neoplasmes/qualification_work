e5f86265cda9b6dc7afd0670a26713d461408507
egor zverkov <quiqqiqiu@gmail.com>
2026-03-10 20:52:30 +0300

    server initialized

---
+++ apps/server/src/index.ts
@@ -0,0 +1,23 @@
+// Import the framework and instantiate it
+import Fastify from 'fastify';
+const fastify = Fastify({
+    logger: true,
+});
+
+// Declare a route
+fastify.get('/', async function handler(request, reply) {
+    return { hello: 'world' };
+});
+
+// Run the server!
+const start = async () => {
+    try {
+        await fastify.listen({ port: 3000 });
+        console.log('server runs');
+    } catch (err) {
+        fastify.log.error(err);
+        process.exit(1);
+    }
+};
+
+start();
